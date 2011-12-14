a35049b20824db54fa9bd4e929b2ff0bf1f20305
lukasz.karluk <julapy@googlemail.com>
2011-12-14 15:06:31 +1100

    first commit

---
+++ src/ofxQCAR.h
@@ -0,0 +1,18 @@
+//
+//  ofxQCAR.h
+//  emptyExample
+//
+//  Created by lukasz karluk on 14/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+class ofxQCAR
+{
+public:
+    
+     ofxQCAR ();
+    ~ofxQCAR ();
+    
+};
---
+++ src/ofxQCAR.mm
@@ -0,0 +1,19 @@
+//
+//  ofxQCAR.cpp
+//  emptyExample
+//
+//  Created by lukasz karluk on 14/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ofxQCAR.h"
+
+ofxQCAR :: ofxQCAR ()
+{
+    //
+}
+
+ofxQCAR :: ~ofxQCAR ()
+{
+    //
+}
