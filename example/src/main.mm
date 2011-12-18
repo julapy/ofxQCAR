df1d1916bb4c2e09f369477d6c77919d835f09f6
lukasz.karluk <julapy@googlemail.com>
2011-12-19 00:17:28 +1100

    enabled depth buffer.

--- example/src/main.mm
+++ example/src/main.mm
@@ -3,6 +3,9 @@
 
 int main()
 {
-    ofSetupOpenGL( 1024,768, OF_FULLSCREEN );
+    ofAppiPhoneWindow *window = new ofAppiPhoneWindow();
+    window->enableDepthBuffer();
+    
+    ofSetupOpenGL( ofPtr<ofAppBaseWindow>( window ), 1024,768, OF_FULLSCREEN );
     ofRunApp( new testApp() );
 }
