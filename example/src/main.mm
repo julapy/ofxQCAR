550a3e24497d3661e66c16a8c77a0bee03359701
lukasz.karluk <julapy@googlemail.com>
2012-02-07 16:56:35 +1100

    added retina support to example.

--- example/src/main.mm
+++ example/src/main.mm
@@ -5,6 +5,9 @@
 int main()
 {
     ofxiOSWindow *iosWindow = new ofxiOSWindow();
+    iosWindow->enableDepthBuffer();
+    iosWindow->enableRetinaSupport();
+    
     ofSetupOpenGL( ofPtr<ofAppBaseWindow>( iosWindow ), 1024,768, OF_FULLSCREEN );
     iosWindow->startAppWithDelegate( "MyAppDelegate" );
 }
