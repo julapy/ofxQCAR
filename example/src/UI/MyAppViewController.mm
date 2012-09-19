d618edafd25c108197172178aa53c5f2503ec8e8
lukasz.karluk <julapy@googlemail.com>
2012-09-20 08:14:37 +1000

    example now launches in different orientation depending on value set in Supported Device Orinetations.

--- example/src/UI/MyAppViewController.mm
+++ example/src/UI/MyAppViewController.mm
@@ -9,27 +9,57 @@
 #import "ofxQCAR_ViewController.h"
 #import "testApp.h"
 
+@interface MyAppViewController() {
+    int orientation;
+}
+@end
+
 @implementation MyAppViewController
+
+- (id)init {
+    self = [super init];
+    if(self) {
+        orientation = -1;
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
 
-- (IBAction)button1Pressed:(id)sender {
-    ofxQCAR_ViewController * viewController;
-    viewController = [[[ofxQCAR_ViewController alloc] initWithAppInPortraitMode:new testApp()] autorelease];
-    [self launchWithQCARViewController:viewController];
-}
+- (IBAction)launchButtonPressed:(id)sender {
 
-- (IBAction)button2Pressed:(id)sender {
     ofxQCAR_ViewController * viewController;
-    viewController = [[[ofxQCAR_ViewController alloc] initWithAppInLandscapeMode:new testApp()] autorelease];
-    [self launchWithQCARViewController:viewController];
-}
+    if(UIInterfaceOrientationIsPortrait(orientation)) {
+        viewController = [[[ofxQCAR_ViewController alloc] initWithAppInPortraitMode:new testApp()] autorelease];
+    } else if(UIInterfaceOrientationIsLandscape(orientation)) {
+        viewController = [[[ofxQCAR_ViewController alloc] initWithAppInLandscapeMode:new testApp()] autorelease];
+    }
 
-- (void)launchWithQCARViewController:(ofxQCAR_ViewController *)viewController {
     [self.navigationController pushViewController:viewController animated:YES];
     self.navigationController.navigationBar.topItem.title = @"Qualcomm AR";
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    /**
+     *  orientation value is originally set to -1
+     *  the very first time this method is called,
+     *  is when it tries to set the orientation to
+     *  the value of Supported Device Orientations.
+     *  so the first value is saved,
+     *  and locked off to that orientation.
+     */
+    if(orientation == -1) {
+        orientation = toInterfaceOrientation;
+        return YES;
+    }
+    
+    return (UIInterfaceOrientation)orientation == toInterfaceOrientation;
 }
 
 @end
