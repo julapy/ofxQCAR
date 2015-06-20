f56ec898493aaa63d002fdeca203d7b478fe95e3
lukasz karluk <julapy@googlemail.com>
2015-06-20 08:48:42 +0200

    glview draw fix with commented exaplanation.

--- src/ofxQCAR_EAGLView.mm
+++ src/ofxQCAR_EAGLView.mm
@@ -14,9 +14,17 @@
 }
 
 - (void)renderFrameQCAR {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self drawView];
-    });
+//  Previously the glview was being drawn on the main thread.
+//  But now with the Vuforia 4.2.3, this was locking up the main thread,
+//  which resulted with native UI being very slow and sometime unresponsive.
+//  glview is now being rendered on another thread which is managed by Vuforia.
+//  Below is the old code, in case in the future it needs to be reverted.
+    
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        [self drawView];
+//    });
+    
+    [self drawView];
 }
 
 - (void)stopAnimation {
